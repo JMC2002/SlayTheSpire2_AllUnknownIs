97526e22a3ebbf0a090bf9f25e58ce8d6740152e
MC J <jmc8@qq.com>
2026-03-18 16:55:03 +0800

    更改打包逻辑，适配0.99.1更新（前几天弄的好像没弄好）

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@
 public static class VersionInfo
 {
     public const string Name = "AllUnknownIs";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
